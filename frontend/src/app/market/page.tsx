0649d005d7f4d4203e03d2b446938560d399d343
abhishek-01k <abhishek@push.org>
2025-02-22 11:07:46 +0530

    fixed the frontend

---
+++ frontend/src/app/market/page.tsx
@@ -0,0 +1,8 @@
+import { EnhancedPredictionMarketDashboard } from "@/components/enhanced-prediction-market-dashboard";
+import React from "react";
+
+const MarketPage = () => {
+  return <EnhancedPredictionMarketDashboard />;
+};
+
+export default MarketPage;
