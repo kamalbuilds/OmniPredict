f33a1cc3ff4e4b593b7cb69df358634b027657c5
kamalbuilds <geniusamansingh@gmail.com>
2025-03-07 17:13:06 +0530

    fix and deploy the contracts on the sonic chain

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -2,5 +2,19 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.28",
+        settings: {
+          evmVersion: 'paris',
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
 };
